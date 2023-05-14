36d110d6a60b96ada6393da2547c249c26bf7868
@KomanDante999! <dreftware@yandex.ru>
2023-05-14 20:48:34 +0700

    lesson 1

--- 01/1-2_Drag_and_Group/drag.js
+++ 01/1-2_Drag_and_Group/drag.js
@@ -1,41 +1,32 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('body');
-    container.classList.add('container');
-
-    const dragDiv = document.createElement('div');
-    dragDiv.classList.add('drag-element');
-    dragDiv.style.left = 0;
-    dragDiv.style.top = 0;
-
-    dragDiv.textContent = 'Drag me!!!'
-    container.append(dragDiv);
-
+    const dragDiv = document.getElementById("drag-element");
     let x, y, left, top;
 
     function onMove(event) {
+      console.log('start move');
       event.preventDefault();
       dragDiv.style.left = event.clientX - x + left;
       dragDiv.style.top = event.clientY - y + top;
     }
 
     // нажатие на элемент
-    dragDiv.addEventListener('mousedown', event => {
+    dragDiv.addEventListener("mousedown", (event) => {
       x = event.clientX;
-      console.log('clientX', x);
+      console.log("clientX", x);
       y = event.clientY;
-      console.log('clientY', y);
+      console.log("clientY", y);
 
       left = parseInt(dragDiv.style.left);
       top = parseInt(dragDiv.style.top);
-      console.log('left', left);
-      console.log('top', top);
-      window.addEventListener('mousemove', onMove);
-    })
+      console.log("left", left);
+      console.log("top", top);
+      window.addEventListener("mousemove", onMove);
+    });
     // отжатие кнопки мыши
-    document.addEventListener('mouseup', () => {
-      window.removeEventListener('mousemove', onMove);
-    })
+    document.addEventListener("mouseup", () => {
+      window.removeEventListener("mousemove", onMove);
+    });
 
 
 
